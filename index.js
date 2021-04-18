fc0b60017e5e00fbb96f86355b7961714b0cc619
David Bobritsky <dvdbobr@gmail.com>
2021-04-18 15:45:39 +0300

    second commit

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 const userRoute = require('./routes/users.route');
-// const roomRoute = require('./routes/room.route');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/users', userRoute);
-// app.use('/api/rooms', roomRoute);
+app.get('/', (req, res) => {
+    res.json({ success: { id: 1, email: 'asfasf@asfasf.com' } })
+})
 
 
-app.listen(port, () => {
-    console.log(`application start at ${port}`)
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`application start at 5000`)
 })
